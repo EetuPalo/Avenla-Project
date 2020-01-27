c6865376672b2f8af7f13f7289ce9ce0924f4eaa
EetuPalo <eetu.palo@student.lab.fi>
2020-01-27 11:32:46 +0200

    Added new registration form and RegisterVM

--- Login_System/ViewModels/RegisterVM.cs
+++ Login_System/ViewModels/RegisterVM.cs
@@ -2,10 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace Login_System.ViewModels
 {
     public class RegisterVM
     {
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EMail { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and password confirmation do not match!")]
+        public string ConfirmPassword { get; set; }
     }
 }
