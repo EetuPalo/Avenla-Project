456ad02fa4e0ff1a652e3a6b6af6b09afeadf6b7
Parviz Azimov <Parviz.Azimov@student.lab.fi>
2020-01-27 12:29:57 +0200

    login page works

--- Login_System/ViewModels/LoginVM.cs
+++ Login_System/ViewModels/LoginVM.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,11 @@ namespace Login_System.ViewModels
 {
     public class LoginVM
     {
+        [Required]
+        [DataType(DataType.Text)]
         public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
