d502abe9877af1fe635e05d5f1dc874f8e9a9f17
Parviz Azimov <Parviz.Azimov@student.lab.fi>
2020-01-29 09:48:19 +0200

    employee controller and model

---
+++ Login_System/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_System.Models
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Telephone { get; set; }
+
+        //Comment
+    }
+}
