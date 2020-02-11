2c25808461ac5d5a53338d9eb7de4382afe26ef4
Parviz Azimov <Parviz.Azimov@student.lab.fi>
2020-02-11 09:06:46 +0200

    group member class

---
+++ Login_System/Models/GroupMember.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_System.Models
+{
+    public class GroupMember
+    {
+        public int Id { get; set; }
+        public int UserID { get; set; }
+
+        public int GroupID { get; set; }
+    }
+}
