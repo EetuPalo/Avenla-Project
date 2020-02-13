35d138e1c0681f6932f46c439f0c6b20fad72eae
EetuPalo <eetu.palo@student.lab.fi>
2020-02-13 10:56:26 +0200

    Moved the evaluation status to the listbydate page

---
+++ Login_System/ViewModels/DateListVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_System.ViewModels
+{
+    public class DateListVM
+    {
+        public string Date { get; set; }
+
+        public string AdminEval { get; set; }
+    }
+}
