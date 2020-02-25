5b251d422f6a7bcd3a7f2a42f68d8ac34a0f24dc
Parviz Azimov <Parviz.Azimov@student.lab.fi>
2020-02-25 10:05:12 +0200

    additonal properties for the skillcoursemember model

--- Login_System/Models/SkillCourseMember.cs
+++ Login_System/Models/SkillCourseMember.cs
@@ -14,7 +14,8 @@ namespace Login_System.Models
         public int UserID { get; set; }
         public string UserName { get; set; }
         public string CourseName { get; set; }
-        //public string Status { get; set; } //In-progress, Completed, Dropout, Planning
+        public string Status { get; set; } //In-progress, Completed, Dropout, Planning
+        public DateTime CompletionDate { get; set; }
         [NotMapped]
         public IEnumerable<SelectListItem> Uname { get; set; }
 
