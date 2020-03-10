75d6e9b835231cca6b31f4198c09cf8ee17c7dab
EetuPalo <eetu.palo@student.lab.fi>
2020-03-10 09:42:45 +0200

    Created Certificates and UserCertificate basic files. UPDATE DATABASE WITH "CertificateDataContext" and "UserCertificateDataContext".

---
+++ Login_System/Models/Certificate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_System.Models
+{
+    public class Certificate
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Organization { get; set; }
+    }
+}
---
+++ Login_System/Models/UserCertificateDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login_System.Models
+{
+    public class UserCertificateDataContext : DbContext
+    {
+        public UserCertificateDataContext(DbContextOptions<UserCertificateDataContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<UserCertificate> UserCertificates { get; set; }
+    }
+}
