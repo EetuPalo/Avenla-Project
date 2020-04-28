e6135328fed3a5a3b59724c7ceda59af2b0f49b8
Eetu Palo <Eetu.Palo@student.lab.fi>
2020-04-28 11:51:45 +0300

    Localization to groups, skillgoals

--- Login_System/Models/Group.cs
+++ Login_System/Models/Group.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Login_System.Models
     {
         public int id { get; set; }
 
+        [Display(Name = "MODEL_Name", ResourceType = typeof(Resources.Groups))]
         public string name { get; set; }
 
         [NotMapped]
--- Login_System/Models/GroupMember.cs
+++ Login_System/Models/GroupMember.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,17 @@ namespace Login_System.Models
     public class GroupMember
     {
         public int Id { get; set; }
+
+        [Display(Name = "MODEL_UserID", ResourceType = typeof(Resources.Groups))]
         public int UserID { get; set; }
+
+        [Display(Name = "MODEL_GroupID", ResourceType = typeof(Resources.Groups))]
         public int GroupID { get; set; }
 
+        [Display(Name = "MODEL_UserName", ResourceType = typeof(Resources.Groups))]
         public string UserName { get; set; }
+
+        [Display(Name = "MODEL_GroupName", ResourceType = typeof(Resources.Groups))]
         public string GroupName { get; set; }
         [NotMapped]
         public IEnumerable<SelectListItem> Group { get; set; }
