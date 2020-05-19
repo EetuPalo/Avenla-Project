8952f32551bfa440c56b1bd46ba996211ce044d7
nikojun <niko.juntunen@student.lamk.fi>
2020-05-19 12:34:07 +0300

    Some minor updates for advanced search.

--- Login_System/Controllers/AdvancedSearchController.cs
+++ Login_System/Controllers/AdvancedSearchController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Login_System.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
 
 namespace Login_System.Controllers
 {
     public class AdvancedSearchController : Controller
     {
-        public IActionResult Index()
+        private readonly GeneralDataContext _context;
+
+        public AdvancedSearchController(GeneralDataContext items)
         {
-            return View();
+            _context = items;
+            
+        }
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var items = from i in _context.Skills select i;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                items = items.Where(s => s.Skill.Contains(searchString));
+            }
+            return View(await items.ToListAsync());
         }
     }
 }
