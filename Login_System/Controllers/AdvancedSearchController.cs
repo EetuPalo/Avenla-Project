81f2ed738531876083637f30786caf1b8985ad5f
nikojun <niko.juntunen@student.lamk.fi>
2020-05-27 10:08:40 +0300

    Cleaned some code and some minor updates to dashboard

--- Login_System/Controllers/AdvancedSearchController.cs
+++ Login_System/Controllers/AdvancedSearchController.cs
@@ -44,7 +44,6 @@ namespace Login_System.Controllers
             var user = await UserMgr.GetUserAsync(HttpContext.User);
             ViewBag.CurrentCompany = user.Company;
 
-
             //Populating the dropdown with certificates
             foreach (var certificate in _context.Certificates)
             {
@@ -62,12 +61,6 @@ namespace Login_System.Controllers
             {
                 model.SkillList.Add(new SelectListItem() { Text = skill.Skill, Value = skill.Skill });
             }
-
-
-
-            //SkillFilter(userList, Skill, min, max);
-            //CertificateFilter(userList, Certificate);
-            //GroupFilter(userList, Groups);
 
             switch (Skill)
             {
@@ -137,7 +130,7 @@ namespace Login_System.Controllers
             return View(model);
         }
 
-
+        // Skill Filter
         public List<AppUser> SkillFilter(List<AppUser> SkillList, string Skill, int? min, int? max)
         {
 
@@ -180,6 +173,7 @@ namespace Login_System.Controllers
             }
             return SkillList;
         }
+        // Certificate Filter
         public List<AppUser> CertificateFilter(List<AppUser> CertList, string Certificate)
         {
             List<AppUser> tempList = new List<AppUser>();
@@ -198,29 +192,9 @@ namespace Login_System.Controllers
                     }
                 }
             }
-
-            
-
-            // this is only if only filter used is Certificate filter
-            //if (userList.Count == 0)
-            //{
-            //    return userList;
-            //}
-
-            //else
-            //{
-            //    for (int i = userList.Count() - 1; i >= 0; i--)
-            //    {
-            //        if (!tempList.Contains(userList[i]))
-            //        {
-            //            userList.Remove(userList[i]);
-            //        }
-            //    }
-            //}
-            
-
             return CertList;
         }
+        // Group Filter
         public List<AppUser> GroupFilter(List<AppUser> GroupList, string Groups)
         {
             List<AppUser> tempList = new List<AppUser>();
@@ -229,8 +203,7 @@ namespace Login_System.Controllers
                              where i.name == Groups
                              select i;
 
-            //if (!string.IsNullOrEmpty(Groups))
-            //{
+
             foreach (var Uname in _context.GroupMembers.Where(x => x.GroupName == Groups))
             {
                 foreach (AppUser user in UserMgr.Users.Where(x => x.Id == Uname.UserID))
@@ -242,106 +215,10 @@ namespace Login_System.Controllers
                     }
                 }
             }
-
-            //}
-            // this is only if only filter used is group filter
-            //if (userList.Count == 0)
-            //{
-            //    return tempList;
-            //}
-
-            //else
-            //{
-            //    for (int i = userList.Count() - 1; i >= 0; i--)
-            //    {
-            //        if (!tempList.Contains(userList[i]))
-            //        {
-            //            userList.Remove(userList[i]);
-            //        }
-            //    }
-            //}
-
             return GroupList;
         }
 
     }
 
 }
-
-//TODO:
-//SearchString should be replaced with a dropdown that's populated with all the skills that are in the database
-//Later there should also be an option to select multiple skills for the search
-//Next to add similar functions for:
-//Filter by skill level (both min and max)
-
-//Note that all these different forms need to be available at the same time and selected/deselected as the user wants
-
-//            if (!string.IsNullOrEmpty(Skill))
-//            {
-//                userList = SkillFilter(userList, Skill, min, max);
-//            }
-
-
-//            if (!string.IsNullOrEmpty(Groups))
-//            {
-//                userList = GroupFilter(userList, Groups);
-
-//            }
-
-//            if (!string.IsNullOrEmpty(Certificate))
-//            {
-//                userList = CertificateFilter(userList, Certificate);
-//            }
-
-
-//            /*if (!string.IsNullOrEmpty(SkillList))
-//            {
-//                items = items.Where(x => x.Skill == SkillList);
-//            }*/
-
-
-//            // Search by Group
-
-
-
-//            // Search by Certificates
-
-//            model.Users = userList;
-
-
-//            // List Certificates
-
-//            var tempList = new List<Certificate>();
-
-//            //Populating the dropdown with certificates
-//            foreach (var certificate in _context.Certificates)
-//            {
-//                model.CertificateList.Add(new SelectListItem() { Text = certificate.Name, Value = certificate.Name });
-//            }
-
-//            // List Groups
-
-//            var tempGroups = new List<Group>();
-
-//            // Populating the dropdown with groups
-//            foreach (var group in _context.Group)
-//            { 
-//            model.GroupList.Add(new SelectListItem() { Text = group.name, Value = group.name });
-//            }
-
-//            // List Skills
-
-//            var tempSkills = new List<Group>();
-
-//            // Populating the dropdown with skills
-//            foreach (var skill in _context.Skills)
-//            {
-//                model.SkillList.Add(new SelectListItem() { Text = skill.Skill, Value = skill.Skill });
-//            }vv
-
-//            // Return
-//            return View(model);
-
-//        }
-
 
