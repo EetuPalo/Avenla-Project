c6705993a8068d1f79f06345047a9105ee22af83
jphpue <otto.kyllonen94@gmail.com>
2020-08-26 10:53:51 +0300

    Datatables changes language based on site language. also fixed caret in advanced search

--- Login_System/wwwroot/js/site.js
+++ Login_System/wwwroot/js/site.js
@@ -4,11 +4,30 @@
 // Write your JavaScript code.
 
 $(document).ready(function () {
-    $('.table').DataTable({
-        stateSave: true
-    });
+    if (document.getElementsByName("culture")[0].selectedOptions[0].value == "fi-FI") {
+        $('.table').DataTable({
+            stateSave: true,
+            language: {
+
+                "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/Finnish.json"
+            }
+
+        });
+    }
+    else if (document.getElementsByName("culture")[0].selectedOptions[0].value == "en-GB") {
+        $('.table').DataTable({
+            stateSave: true,
+            language: {
+
+                "url": "//cdn.datatables.net/plug-ins/1.10.21/i18n/English.json"
+            }
+
+        });
+    }
+
     $('.disable_arrow').css("background", "none");
 });
+
 
 function goBack() {
     window.history.back();
